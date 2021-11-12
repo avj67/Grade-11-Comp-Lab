4bcfc03b0601bcd9f0c5700d36cfc8238b2f7705
Laksh Bharani <80910691+LakshBharani@users.noreply.github.com>
2021-11-12 12:09:05 +0530

    Update 4.py

--- Prac assignment 5/Code/4.py
+++ Prac assignment 5/Code/4.py
@@ -1,40 +1,23 @@
 # form plaindromes from words entered
 
-words = []
-temp_word = ""
-output_li_index = []
-output_li_pal = []
-j = 0
+n = int(input("Number of words to be entered: ")) 
+input_list = [] 
+output_index = []
+output_palin = []
 
-n = int(input("How many words: "))
+for i in range (n): 
+          word = input("Enter the word: ") 
+          input_list.append(word) 
 
+for j in range (len(input_list)): 
+    for k in range (len(input_list)): 
+        if (k!=j): 
+            new_word = input_list[j]+input_list[k] 
+            if (new_word == new_word[::-1]): 
+                new_word_list = [j,k] 
+                output_index.append(new_word_list) 
+                output_palin.append(new_word) 
 
-# accept input
-for i in range(n):
-    word = input("Input: ")
-    words.append(word)
+print("Output:",output_index)
+print("Explanation: The panidromes are",output_palin)
 
-for j in range (n):
-    if j==0:
-        for k in range (j+1,n):
-            temp_word=words[j]+words[k]
-            if temp_word == temp_word[::-1]:
-                output_li_index.append("[",j,k,"]")
-                output_li_pal.append(words[j]+words[k])
-        
-    else:
-        for l in range (0,j):
-            temp_word=words[j]+words[k]
-            if temp_word == temp_word[::-1]:
-                output_li_index.append("[",j,k,"]")
-                output_li_pal.append(words[j]+words[k])
-
-        for m in range (j+1,n):
-            temp_word=words[j]+words[k]
-            if temp_word == temp_word[::-1]:
-                output_li_index.append("[",j,k,"]")
-                output_li_pal.append(words[j]+words[k])
-        
-
-print("Output:",output_li_index)
-print("The palindromes are",output_li_pal)
