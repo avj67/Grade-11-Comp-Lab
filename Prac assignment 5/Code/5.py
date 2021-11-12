968f1f89c7ab2d514312101f9a084ce5a04e7f20
Laksh Bharani <80910691+LakshBharani@users.noreply.github.com>
2021-11-12 12:10:51 +0530

    Update 5.py

--- Prac assignment 5/Code/5.py
+++ Prac assignment 5/Code/5.py
@@ -1,17 +1,21 @@
 # print pattern
 
-n = int(input("Enter n: "))
-li = []
+n = int(input("Enter a number:")) 
+outer_li = [] 
+row = 0 
+inner_li = [] 
+i = 0 
 
-for num in range(n):
-    li.append(num)
+for i in range (n): 
+    j = i 
+    for j in range (i, n): 
+        inner_li.append(j) 
+    for j in range (i): 
+        inner_li.append(j) 
+    outer_li.append(inner_li) 
+    inner_li = [] 
 
-for i in range (len(li)):
-    for j in range (i, len(li)):
-        print(li[j], end = ' ')
-
-    for k in range (i):
-        print(li[k], end = ' ')
-    
-    print()
-
+for i in range (len(outer_li)): 
+    for j in range (len(outer_li[i])): 
+        print (outer_li[i][j], end = ' ') 
+    print ("")
