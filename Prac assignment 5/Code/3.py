11e7989a532326ae4f1df6b8f42d0f3ba473eb9a
Laksh Bharani <80910691+LakshBharani@users.noreply.github.com>
2021-11-12 13:07:28 +0530

    Update 3.py

--- Prac assignment 5/Code/3.py
+++ Prac assignment 5/Code/3.py
@@ -1,33 +1,19 @@
 # Accept nums from user and display longest sequence of odd numbers as a tuple
 
-li = []
-count = 0
-li_temp = []
-li_new=[]
-li_final = []
+longest = []
+current = []
 
-n = int(input("How many nums: "))
+while True:
+    n = int(input("Enter num: "))
+    if n == -1:
+        del longest[-1]
+        break
 
-# accept input
-for i in range(n):
-    num = int(input("Input: "))
-    li.append(num)
-t = tuple(li)
-
-for j in range(n):
-    if t[j]%2 == 1:
-        li_new.append(t[j])
-        temp_count = 1
-        count += 1
-        if temp_count>=count:
-            li_final=li_new
-
-# reset values if num is even
+    if current and current[-1] % 2 == 1:
+        current.append(n)
     else:
-        temp_count = count
-        li_temp=li_new
-        li_new=[]
-        count=0
+        current = [n]
 
-print ("input:",t)
-print("Longest sequence of odd numbers in the tuple:",tuple(li_final))
+    if len(current) > len(longest):
+        longest = current
+print(longest)
